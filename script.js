d1fc0dd43996d9ebe5176d1ea96a73afa1026be7
Vladislav Surin <vlad_surin@mail.ru>
2022-10-26 02:03:49 +0300

    completed third practice

--- script.js
+++ script.js
@@ -10,3 +10,79 @@ false - выводит в консоль главный объект прогр�
 genres
 
 P.S. Функции вызывать не обязательно*/
+
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", "").trim();
+    const gradeFilm = prompt("На сколько оцените его?", "").trim();
+
+    if (
+      lastFilm !== null &&
+      gradeFilm !== null &&
+      lastFilm !== "" &&
+      lastFilm.length < 51 &&
+      gradeFilm !== ""
+    ) {
+      personalMovieDB.movies[lastFilm] = gradeFilm;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB(obj) {
+  if (!obj.privat) {
+    console.log(obj);
+  }
+}
+
+showMyDB(personalMovieDB);
+
+function writeYourGenres(obj) {
+  for (let i = 1; i <= 3; i++) {
+    const answer = prompt(`Ваш любимый жанр под номером ${i}`, "");
+    obj.genres.push(answer);
+  }
+}
+
+writeYourGenres(personalMovieDB);
