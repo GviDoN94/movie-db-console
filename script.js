5e61b7161762651d0a123c89b4b97a83b5fecdfe
Vladislav Surin <vlad_surin@mail.ru>
2022-10-27 00:18:34 +0300

    started fourth practice

--- script.js
+++ script.js
@@ -1,88 +1,17 @@
 /* Задание на урок:
 
-1) Первую часть задания повторить по уроку
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку.
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены -
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-P.S. Функции вызывать не обязательно*/
+'use strict';
 
-"use strict";
-
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-  }
-}
-
-start();
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const lastFilm = prompt("Один из последних просмотренных фильмов?", "").trim();
-    const gradeFilm = prompt("На сколько оцените его?", "").trim();
-
-    if (
-      lastFilm !== null &&
-      gradeFilm !== null &&
-      lastFilm !== "" &&
-      lastFilm.length < 51 &&
-      gradeFilm !== ""
-    ) {
-      personalMovieDB.movies[lastFilm] = gradeFilm;
-      console.log("done");
-    } else {
-      console.log("error");
-      i--;
-    }
-  }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
-    console.log("Просмотрено довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
-
-detectPersonalLevel();
-
-function showMyDB(obj) {
-  if (!obj.privat) {
-    console.log(obj);
-  }
-}
-
-showMyDB(personalMovieDB);
-
-function writeYourGenres(obj) {
-  for (let i = 1; i <= 3; i++) {
-    const answer = prompt(`Ваш любимый жанр под номером ${i}`, "");
-    obj.genres.push(answer);
-  }
-}
-
-writeYourGenres(personalMovieDB);
+// Код возьмите из предыдущего домашнего задания
